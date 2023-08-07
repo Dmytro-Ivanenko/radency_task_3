f4a7c2580c9d58bdf1bd61b43481c9000acb687d
Dmytro-Ivanenko <dmitro0399@gmail.com>
2023-08-07 21:34:42 +0300

    add getNotesStats

--- src/repositories/controlers.ts
+++ src/repositories/controlers.ts
@@ -1,8 +1,41 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
+import HttpError from '../helpers/HttpError.js';
 import notesData from '../data/notes.js';
+import { calculateCategoryStats } from '../services/statFunctions.js';
+
+const memoryStorage = [...notesData];
+
 // get
 const getAllNotes = async (req: Request, res: Response) => {
-	res.status(200).json(notesData);
+	res.status(200).json(memoryStorage);
 };
 
-export { getAllNotes };
+const getNote = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const noteId: string = req.params.id;
+		const note = memoryStorage.find((item) => item.id === noteId);
+
+		if (!note) {
+			throw HttpError(404, `Note with id ${noteId} not found.`);
+		}
+
+		res.status(200).json(note);
+	} catch (error) {
+		next(error);
+	}
+};
+
+const getNotesStats = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	try {
+		const stats = calculateCategoryStats(memoryStorage);
+		res.status(200).json(stats);
+	} catch (error) {
+		next(error);
+	}
+};
+
+export { getAllNotes, getNote, getNotesStats };
--- src/routes/notesRouter.ts
+++ src/routes/notesRouter.ts
@@ -1,13 +1,18 @@
 import express from 'express';
-import { getAllNotes } from '../repositories/controlers.js';
+import {
+	getAllNotes,
+	getNote,
+	getNotesStats,
+} from '../repositories/controlers.js';
 // import validateBody from '../services/validateBody.js';
 // import { noteValidateSchema } from '../models/schemas.js';
 const router = express.Router();
 
 // routes
-// router.get('/:id', getNote);
+
 router.get('/', getAllNotes);
-// router.get('/stats', getNotesStats);
+router.get('/stats', getNotesStats);
+router.get('/:id', getNote);
 // router.post('/', validateBody(noteValidateSchema), postNote);
 // router.delete('/:id', deleteNote);
 // router.patch('/:id', validateBody(noteValidateSchema), updateNote);
